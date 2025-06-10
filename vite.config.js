3b2370794159e0b4f95b933890a4d1efec917558
Kircheva Boryana (LDC) <boryana.kircheva@liebherr.com>
2025-06-10 23:23:29 +0300

    specify output directory

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // За да работи __dirname
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "dist", // Specify the output directory for the production build
+    assetsDir: "", // Assets directory relative to outDir
+    // You can add more build options as needed
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
